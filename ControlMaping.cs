1145f49a5b1fe9cd29cfe2347c8288fc18e16cd6
jbodoh7613 <60298326+jbodoh7613@users.noreply.github.com>
2023-02-10 12:38:02 -0500

    Added member variables to ControlMaping class

--- ControlMaping.cs
+++ ControlMaping.cs
@@ -8,12 +8,28 @@ namespace WindowsFormsApp3
 {
     class ControlMaping
     {
-
         int ControlMode = 1;
-
-
-
-
-
+        string leftJoyX = "";
+        bool isLeftJoyXInverted = false;
+        string leftJoyY = "";
+        bool isLeftJoyYInverted = false;
+        string rightJoyX = "";
+        bool isRightJoyXInverted = false;
+        string rightJoyY = "";
+        bool isRightJoyYInverted = false;
+        string potent1 = "";
+        string potent2 = "";
+        string triSwitch1Pos1 = "";
+        string triSwitch1Pos2 = "";
+        string triSwitch1Pos3 = "";
+        string triSwitch2Pos1 = "";
+        string triSwitch2Pos2 = "";
+        string triSwitch2Pos3 = "";
+        string toggleSwitch1Pos1 = "";
+        string toggleSwitch1Pos2 = "";
+        string toggleSwitch2Pos1 = "";
+        string toggleSwitch2Pos2 = "";
     }
 }
+
+
