5b024da1e696d96df313284b26f7b6dd81ca3d83
jbodoh7613 <60298326+jbodoh7613@users.noreply.github.com>
2023-02-20 12:14:41 -0500

    Add classes for control settings to ControlMaping.cs

--- ControlMaping.cs
+++ ControlMaping.cs
@@ -6,9 +6,64 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApp3
 {
-    class ControlMaping
+    public class JoyDirSetting
     {
-        int ControlMode = 1;
+        public int joySetting = 0;
+        public int isInverted = 0;
+
+        JoyDirSetting(int joySetting, int isInverted)
+        {
+            this.joySetting = joySetting;
+            this.isInverted = isInverted;
+        }
+    }
+    public class JoyJson
+    {
+        public JoyDirSetting x;
+        public JoyDirSetting y;
+
+        public JoyJson(JoyDirSetting x, JoyDirSetting y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public class TriJson
+    {
+        public int pos1;
+        public int pos2;
+        public int pos3;
+
+        public TriJson(int pos1, int pos2, int pos3)
+        {
+            this.pos1 = pos1;
+            this.pos2 = pos2;
+            this.pos3 = pos3;
+        }
+    }
+    public class ToggleJson
+    {
+        public int pos1;
+        public int pos2;
+
+        public ToggleJson(int pos1, int pos2)
+        {
+            this.pos1 = pos1;
+            this.pos2 = pos2;
+        }
+    }
+    public class PotentJson
+    {
+        public int setting;
+
+        public PotentJson(int setting)
+        {
+            this.setting = setting;
+        }
+    }
+    public class ControlMaping
+    {
+        int controlMode = 1;
         string leftJoyX = "";
         bool isLeftJoyXInverted = false;
         string leftJoyY = "";
@@ -29,6 +84,32 @@ namespace WindowsFormsApp3
         string toggleSwitch1Pos2 = "";
         string toggleSwitch2Pos1 = "";
         string toggleSwitch2Pos2 = "";
+
+        object joyJsonMap = new Dictionary<string, int>()
+        {
+            {"Yaw", 0},
+            {"Pitch", 1},
+            {"Roll", 2},
+            {"Forward", 3},
+            {"Back", 4},
+            {"Left", 5},
+            {"Right", 6}
+        };
+        object switchJsonMap = new Dictionary<string, int>()
+        {
+            {"Drone E-stop A", 0},
+            {"Drone E-stop B", 1},
+            {"Flight Mode A", 2},
+            {"Flight Mode B", 3},
+            {"High Sensitivity", 4},
+            {"Low Sensitivity", 5}
+        };
+        object potentJsonMap = new Dictionary<string, int>()
+        {
+            {"Drone Sensitivity", 0}
+        };
+        
+        public 
     }
 }
 
