0cc88b29a10bad25cadeb5295d8ea2af52f334b3
jbodoh7613 <60298326+jbodoh7613@users.noreply.github.com>
2023-02-23 18:45:29 -0500

    Finished toJsn() function in ControlMaping

--- ControlMaping.cs
+++ ControlMaping.cs
@@ -30,17 +30,38 @@ namespace WindowsFormsApp3
         public string toggleSwitch2Pos1 = "";
         public string toggleSwitch2Pos2 = "";
 
-
-
         public string stringToNum(string s) {
             if (s == "yaw") { return 3.ToString(); }
             
             return ""; 
         }
+        public string boolToNum(bool b)
+        {
+            return b ? 1.ToString() : 0.ToString();
+        }
         public string toJsn() {
             string s = "{" +
-            "leftJoyX" + stringToNum(leftJoyX) + "," +
-            "leftJoyY" + stringToNum(leftJoyY) + "," +
+            "\"controlMode\": " + controlMode.ToString() + ", " +
+            "\"leftJoyX\": " + stringToNum(leftJoyX) + ", " +
+            "\"isLeftJoyXInverted\": " + boolToNum(isLeftJoyXInverted) + ", " +
+            "\"leftJoyY\": " + stringToNum(leftJoyY) + ", " +
+            "\"isLeftJoyYInverted\": " + boolToNum(isLeftJoyYInverted) + ", " +
+            "\"rightJoyX\": " + stringToNum(rightJoyX) + ", " +
+            "\"isRightJoyXInverted\": " + boolToNum(isRightJoyXInverted) + ", " +
+            "\"rightJoyY\": " + stringToNum(leftJoyX) + ", " +
+            "\"isRightJoyYInverted\": " + boolToNum(isRightJoyYInverted) + ", " +
+            "\"potent1\": " + stringToNum(potent1) + ", " +
+            "\"potent2\": " + stringToNum(potent2) + ", " +
+            "\"triSwitch1Pos1\": " + stringToNum(triSwitch1Pos1) + ", " +
+            "\"triSwitch1Pos2\": " + stringToNum(triSwitch1Pos2) + ", " +
+            "\"triSwitch1Pos3\": " + stringToNum(triSwitch1Pos3) + ", " +
+            "\"triSwitch2Pos1\": " + stringToNum(triSwitch2Pos1) + ", " +
+            "\"triSwitch2Pos2\": " + stringToNum(triSwitch2Pos2) + ", " +
+            "\"triSwitch2Pos3\": " + stringToNum(triSwitch2Pos3) + ", " +
+            "\"toggleSwitch1Pos1\": " + stringToNum(toggleSwitch1Pos1) + ", " +
+            "\"toggleSwitch1Pos2\": " + stringToNum(toggleSwitch1Pos2) + ", " +
+            "\"toggleSwitch2Pos1\": " + stringToNum(toggleSwitch2Pos1) + ", " +
+            "\"toggleSwitch2Pos2\": " + stringToNum(toggleSwitch2Pos2) + ", " +
             "}";
 
             return s; 
