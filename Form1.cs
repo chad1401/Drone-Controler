4c87b54df2a1aec42f282abd0e22922be1fc5e66
jbodoh7613 <60298326+jbodoh7613@users.noreply.github.com>
2023-04-05 12:27:16 -0400

    Moved ControlMaping updating logic to button2_Click

--- Form1.cs
+++ Form1.cs
@@ -55,102 +55,102 @@ namespace WindowsFormsApp3
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            mapping.leftJoyX = comboBox1.SelectedItem.ToString();
+            
         }
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
-            mapping.leftJoyY = comboBox2.SelectedItem.ToString();
+            
         }
         
         private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
         {
-            mapping.rightJoyX = comboBox4.SelectedItem.ToString();
+            
         }
 
         private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
         {
-            mapping.rightJoyY = comboBox3.SelectedItem.ToString();
+            
         }
 
         private void comboBox12_SelectedIndexChanged(object sender, EventArgs e)
         {
-            mapping.triSwitch1Pos1 = comboBox12.SelectedItem.ToString();
+            
         }
 
         private void comboBox11_SelectedIndexChanged(object sender, EventArgs e)
         {
-            mapping.triSwitch1Pos2 = comboBox11.SelectedItem.ToString();
+            
         }
 
         private void comboBox13_SelectedIndexChanged(object sender, EventArgs e)
         {
-            mapping.triSwitch1Pos3 = comboBox13.SelectedItem.ToString();
+            
         }
 
         private void comboBox16_SelectedIndexChanged(object sender, EventArgs e)
         {
-            mapping.triSwitch2Pos1 = comboBox16.SelectedItem.ToString();
+            
         }
 
         private void comboBox15_SelectedIndexChanged(object sender, EventArgs e)
         {
-            mapping.triSwitch2Pos2 = comboBox15.SelectedItem.ToString();
+            
         }
 
         private void comboBox14_SelectedIndexChanged(object sender, EventArgs e)
         {
-            mapping.triSwitch2Pos3 = comboBox14.SelectedItem.ToString();
+            
         }
 
         private void comboBox9_SelectedIndexChanged_1(object sender, EventArgs e)
         {
-            mapping.potent1 = comboBox9.SelectedItem.ToString();
+            
         }
 
         private void comboBox10_SelectedIndexChanged(object sender, EventArgs e)
         {
-            mapping.potent2 = comboBox10.SelectedItem.ToString();
+            
         }
 
         private void comboBox6_SelectedIndexChanged(object sender, EventArgs e)
         {
-            mapping.toggleSwitch1Pos1 = comboBox6.SelectedItem.ToString();
+            
         }
 
         private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
         {
-            mapping.toggleSwitch1Pos2 = comboBox6.SelectedItem.ToString();
+            
         }
 
         private void comboBox7_SelectedIndexChanged(object sender, EventArgs e)
         {
-            mapping.toggleSwitch2Pos1 = comboBox7.SelectedItem.ToString();
+            
         }
 
         private void comboBox8_SelectedIndexChanged(object sender, EventArgs e)
         {
-            mapping.toggleSwitch2Pos2 = comboBox8.SelectedItem.ToString();
+            
         }
 
         private void checkBox5_CheckedChanged(object sender, EventArgs e)
         {
-            mapping.isLeftJoyXInverted = checkBox5.Checked;
+            
         }
 
         private void checkBox6_CheckedChanged(object sender, EventArgs e)
         {
-            mapping.isLeftJoyYInverted = checkBox6.Checked;
+            
         }
 
         private void checkBox8_CheckedChanged(object sender, EventArgs e)
         {
-            mapping.isRightJoyXInverted= checkBox8.Checked;
+            
         }
 
         private void checkBox7_CheckedChanged(object sender, EventArgs e)
         {
-            mapping.isRightJoyYInverted = checkBox7.Checked;
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -185,6 +185,27 @@ namespace WindowsFormsApp3
 
         private void button2_Click(object sender, EventArgs e)
         {
+            mapping.leftJoyX = comboBox1.SelectedItem.ToString();
+            mapping.leftJoyY = comboBox2.SelectedItem.ToString();
+            mapping.rightJoyX = comboBox4.SelectedItem.ToString();
+            mapping.rightJoyY = comboBox3.SelectedItem.ToString();
+            mapping.triSwitch1Pos1 = comboBox12.SelectedItem.ToString();
+            mapping.triSwitch1Pos2 = comboBox11.SelectedItem.ToString();
+            mapping.triSwitch1Pos3 = comboBox13.SelectedItem.ToString();
+            mapping.triSwitch2Pos1 = comboBox16.SelectedItem.ToString();
+            mapping.triSwitch2Pos2 = comboBox15.SelectedItem.ToString();
+            mapping.triSwitch2Pos3 = comboBox14.SelectedItem.ToString();
+            mapping.potent1 = comboBox9.SelectedItem.ToString();
+            mapping.potent2 = comboBox10.SelectedItem.ToString();
+            mapping.toggleSwitch1Pos1 = comboBox6.SelectedItem.ToString();
+            mapping.toggleSwitch1Pos2 = comboBox6.SelectedItem.ToString();
+            mapping.toggleSwitch2Pos1 = comboBox7.SelectedItem.ToString();
+            mapping.toggleSwitch2Pos2 = comboBox8.SelectedItem.ToString();
+            mapping.isLeftJoyXInverted = checkBox5.Checked;
+            mapping.isLeftJoyYInverted = checkBox6.Checked;
+            mapping.isRightJoyXInverted = checkBox8.Checked;
+            mapping.isRightJoyYInverted = checkBox7.Checked;
+
             string jsonString = JsonSerializer.Serialize<ControlMaping>(mapping);
             write(jsonString);
         }
